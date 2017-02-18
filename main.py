344b3b7f9460034563690ab5bad89cc0da41fb62
joemccor <joemccor@gmail.com>
2017-02-17 21:10:08 -0500

    python3 changes

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
 	else:
 		imgPath = sys.argv[1]
 		img = cv2.imread(imgPath, 0)
-	display = raw_input("Image loaded. Display? (y/n): ")
+	display = input("Image loaded. Display? (y/n): ")
 	if display=="y":
 		cv2.startWindowThread()
 		cv2.imshow("Original", img)
@@ -38,7 +38,7 @@ def main():
 
 	#Segment Image
 	segmented = seg.segmentImage(img)
-	display = raw_input("Image has been segmented. Display? (y/n): ")
+	display = input("Image has been segmented. Display? (y/n): ")
 	if display=="y":
 		cv2.startWindowThread()
 		cv2.imshow('Segmented', segmented)
@@ -50,7 +50,7 @@ def main():
 	components = conn.findConnectedComponents()
 	symbols = conn.createComponentMasks()
 	# print(conn.ccCount)
-	raw_input("Found connected components. Press any button to continue.")
+	input("Found connected components. Press any button to continue.")
 
 	#Bounding Boxes (raw and resized)
 	print("\nGathering bounding boxes...")
@@ -59,7 +59,7 @@ def main():
 	for i in range(len(bboxes)):
 		minX, minY, maxX, maxY = bboxes[i]
 		bounded = cv2.rectangle(bounded,(minY, minX),(maxY,maxX),(0,0,200),3,cv2.LINE_8)
-	display = raw_input("Bounding Boxes calculated. Display? (y/n): ")
+	display = input("Bounding Boxes calculated. Display? (y/n): ")
 	if display=="y":
 		cv2.startWindowThread()
 		cv2.imshow('BoundingBoxes', bounded)
@@ -79,13 +79,13 @@ def main():
 				)
 			)
 		))
-	raw_input("All features extracted. Press any button to begin classifying")
+	input("All features extracted. Press any button to begin classifying")
 
 	#SVM Classification
 	syms = d.getDict()
 	print("\nSending features for classification by voting...")
 	preds = svm.voteClassify(features, 5, 5)
-	display = raw_input("Raw classification complete. Display? (y/n): ")
+	display = input("Raw classification complete. Display? (y/n): ")
 	if display=="y":
 		# print("\n--------------- RAW CLASSIFICATIONS ---------------")
 		labeled = bounded
@@ -98,7 +98,7 @@ def main():
 		cv2.waitKey(1)	
 		# print("-------------------- DONE --------------------")
 
-	doGrammar = raw_input("Classification done. Proceed with grammar? (y/n)")
+	doGrammar = input("Classification done. Proceed with grammar? (y/n)")
 	if doGrammar=="y":
 		#XY-cut
 		bboxMap = {}
@@ -116,13 +116,14 @@ def main():
 
 		mathExpression = ''.join(results)
 		mathExpression = mathExpression.replace('X','*')
-		mathAnswer = eval(mathExpression)
-		print( '{}={}'.format(mathExpression,mathAnswer) )
-		#XY-cut
+		print( '{}'.format(mathExpression) )
+		if input("Evaluate?(y/n)") == "y":
+			mathAnswer = eval(mathExpression)
+			print( '{}={}'.format(mathExpression,mathAnswer) )
 		#Symbol combination
 
 	#Program Ended
-	raw_input("End of program. Press any button to quit.")
+	input("End of program. Press any button to quit.")
 
 if __name__=="__main__":
 	main()
